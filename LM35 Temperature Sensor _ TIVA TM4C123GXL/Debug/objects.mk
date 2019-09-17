edd1ab7f92df126c991f75717ff5f7471adf8ef0
Prashant Dandriyal <46529961+PrashantDandriyal@users.noreply.github.com>
2019-09-17 18:41:37 +0530

    Added the Entire Project

---
+++ LM35 Temperature Sensor _ TIVA TM4C123GXL/Debug/objects.mk
@@ -0,0 +1,8 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+USER_OBJS :=
+
+LIBS := -llibc.a -l"C:/ti/TivaWare_C_Series-2.1.4.178/tivaware_c_series_2_1_4_178/driverlib/ccs/Debug/driverlib.lib"
+
